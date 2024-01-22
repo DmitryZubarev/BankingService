13233b68c65fa18590c616536880f54ccb2ff22b
Николай Перминов <kolya-perminov@ya.ru>
2024-01-22 23:59:26 +0500

    Добавьте файлы проекта.

---
+++ GaB_BankingService/ProtectedDbConnector/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using Gab_BankingService.DbConnector.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gab_BankingService.DbConnector;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(Gab_BankingService.ConfigurationManager.ConnectionStringProtectedDb);
+    }
+}
---
+++ GaB_BankingService/ProtectedDbConnector/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Gab_BankingService.DbConnector.Models;
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
